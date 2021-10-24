5c361d14e0e78b32f2078c8ef51be1e9a732af3d
eggyalviano <80298251+eggyalviano@users.noreply.github.com>
2021-10-25 00:31:09 +0700

    init commit

---
+++ rumah kost.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+    <meta charset="utf.8">
+    <meta name="viewport" content="width=device-width, initial-scale-1">
+	<title>EGGY ALVIANO</title>
+    <link rel="stylesheet" type="text/css" href="stylekost.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+</head>
+    <Body>
+        <!--produk-->
+        <div class="gambar">
+            <div class="foto">
+                <img src="mrh.jpg">
+                <h1>kost ibu tuti</h1>
+                <p>Harga Rp.800.000/bulan</p>
+                <p>Fasilitas</p>
+                <p>AC lemari meja belajar kasur</p>
+</div>
+            </div>
+
+        <!--header-->
+
+        <div class="medsos">
+            <div class="container">
+                <ul>
+                    <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                </ul>
+            </div>
+        </div>
+        <header>
+            <div class="container">
+            <h1><a href="coba.html">SEDIA KOST</a></h1>
+            <ul>
+                <li class="aktif"><a href="coba.html">HOME</a></li>
+                <li><a href="tentang.html">BOOKING</a></li>
+                <li><a href="masalah.html">MY BOOKING</a></li>
+                <li><a href="kontak.html">KONTAK</a></li>
+            </div>
+            </ul>
+        </header>
+
+        <!---ini cover-->
+            <section class="bg">
+                <h2>SELAMAT DATANG</h2>
+            </section>
+
+
+            <div class="footer">
+                <p class="copy">EGGY ALVIANO_1202184207 </p>
+            </div>
+    </Body>
+</html>
